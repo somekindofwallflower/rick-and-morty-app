83fa36dc0f81469fe821e59d758d5dd802425b62
somekindofwallflower <someone.ana13@gmail.com>
2022-02-22 22:33:42 +0100

    Create a basic project structure

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import "./Layout.css";
+import Footer from "../components/Footer/Footer"
+import Header from "../components/Header/Header"
+
+const Layout = () => {
+    return (
+        <div>
+            {/*TODO Create app layout*/}
+            {/*Header*/}
+            <Header/>
+            {/*Body*/}
+            Rick and Morty
+            {/*Footer*/}
+            <Footer/>
+        </div>
+    )
+}
+
+export default Layout
