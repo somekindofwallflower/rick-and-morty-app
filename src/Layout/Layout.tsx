8600ea0b38a1058dfae4c6215ff32feeee71d6c8
somekindofwallflower <someone.ana13@gmail.com>
2022-02-23 20:51:03 +0100

    feat: Add React Router routing library

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -2,18 +2,30 @@ import * as React from 'react'
 import "./Layout.css";
 import Footer from "src/components/Footer/Footer"
 import Header from "src/components/Header/Header"
+import Home from "src/modules/Home/Home"
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const Layout = () => {
     return (
-        <div>
+        <BrowserRouter>
             {/*TODO Create app layout*/}
             {/*Header*/}
             <Header/>
             {/*Body*/}
-            Rick and Morty
+            <main>
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    {/*No match route*/}
+                    <Route
+                        path="*"
+                        element={<Navigate to="/home" />}
+                    />
+                </Routes>
+            </main>
             {/*Footer*/}
             <Footer/>
-        </div>
+        </BrowserRouter>
     )
 }
 
